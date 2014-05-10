f61ce49adbdbcbac598c18429cef2b7dadd35f65
Osman Masood <oamasood@gmail.com>
2014-05-10 16:36:41 -0700

    initial test framework

---
+++ test/test.py
@@ -0,0 +1,44 @@
+#!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+# -*- coding: utf-8 -*-
+
+from parse_rest.datatypes import Object
+from parse_rest.connection import register
+
+APP_ID = "qb9AlvfEwVJkZSVCfXOKmxOytM4mHjP8qiuTgamD"
+REST_API_KEY = "MTfw3ouWamMhsgBzLf3wOofRDt1TFUs4Wup1fN9v"
+
+
+class User(Object):
+    pass
+
+
+class Event(Object):
+    pass
+
+
+class EventAttendee(Object):
+    pass
+
+
+class UserContacts(Object):
+    pass
+
+
+class Comment(Object):
+    pass
+
+
+if __name__ == "__main__":
+    register(APP_ID, REST_API_KEY)
+
+    
+
+
+
+
+
+
+
+
+
+
